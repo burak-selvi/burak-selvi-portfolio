6468f79fd01c8a654f7e7f9ec60ee45f2605f5f2
Burak <burakselvi858@hotmail.com>
2020-07-04 23:34:32 +0300

    Updates

---
+++ precache-manifest.f98be6c93775f9b5496141abaeadc02b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af709fa924b30550f3c64f7fe17df34c",
+    "url": "/burak-selvi-portfolio/index.html"
+  },
+  {
+    "revision": "2a5167cd958106f676d8",
+    "url": "/burak-selvi-portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b37915239908c955c027",
+    "url": "/burak-selvi-portfolio/static/js/2.2eae3e20.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burak-selvi-portfolio/static/js/2.2eae3e20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a5167cd958106f676d8",
+    "url": "/burak-selvi-portfolio/static/js/main.9867d9fa.chunk.js"
+  },
+  {
+    "revision": "7cc148c800c5f5de4fcf",
+    "url": "/burak-selvi-portfolio/static/js/runtime-main.c6070d9d.js"
+  }
+]);
